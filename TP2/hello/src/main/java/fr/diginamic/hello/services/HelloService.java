f0bc3405afabdea720f6a5cf8537df17b9a71033
Dmitry <Dmitrykorzhenevskiy@gmail.com>
2025-08-20 12:22:03 +0200

    actions3

--- TP2/hello/src/main/java/fr/diginamic/hello/services/HelloService.java
+++ TP2/hello/src/main/java/fr/diginamic/hello/services/HelloService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
-
+ //password
     public String salutations() {
         return "Je suis la classe de service et je vous dis Bonjour";
     }
