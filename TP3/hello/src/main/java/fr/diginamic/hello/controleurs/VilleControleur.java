f0c407048634be11ce4e2e325d5914827a8d3757
Dmitry <Dmitrykorzhenevskiy@gmail.com>
2025-08-20 14:18:40 +0200

    TP3

---
+++ TP3/hello/src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -0,0 +1,24 @@
+package fr.diginamic.hello.controleurs;
+
+import fr.diginamic.hello.Ville;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/villes")
+public class VilleControleur {
+
+    @GetMapping
+    public List<Ville> getVilles() {
+
+        List<Ville> villes = new ArrayList<>();
+        villes.add(new Ville("Montpellier", 200000));
+        villes.add(new Ville("Perols", 15000));
+
+        return villes;
+    }
+}
